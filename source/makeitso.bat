d066e65cdf4ab7a9c7c8040cd9906f4de5b2dbf3
buzzware <regos@buzzware.com.au>
2010-11-05 21:07:51 +0800

    more files

---
+++ source/makeitso.bat
@@ -0,0 +1,2 @@
+@echo off
+"%PHP_PEAR_PHP_BIN%" "%PHP_PEAR_BIN_DIR%\makeitso" %1 %2 %3 %4 %5 %6 %7 %8 %9
---
+++ source/makeitso_dev.bat
@@ -0,0 +1,2 @@
+@echo off
+"C:\Program Files\PHP\php.exe" "Y:\repos\makeItSo\source\makeitso" %1 %2 %3 %4 %5 %6 %7 %8 %9
